08c8c7500b1f1685b4730a76369b52523c9750ac
Gerem66 <geremy.lecaplain66@gmail.com>
2022-04-05 15:25:35 +0200

    Test: Notifications

--- src/Utils/Notifications.js
+++ src/Utils/Notifications.js
@@ -6,11 +6,22 @@ import user from '../Managers/UserManager';
 import langManager from '../Managers/LangManager';
 import dataManager from '../Managers/DataManager';
 
-import { Random } from './Functions';
+import { Random, Range } from './Functions';
 
-const CHANNELS_ID = {
-    morning: 'morning',
-    evening: 'evening'
+/**
+ * @typedef {Object} Channel
+ * @property {String} ID Channel ID for android
+ * @property {() => { Title, Body }} generate Function to generate notification content
+ * @property {Number} hour Hour of the day [0, 23]
+ * @property {Number} [minutes=0] Minutes of the hour [0, 59]
+ */
+
+/** @returns {Channel} */
+const CreateChannel = (ID, generate, hour, minutes = 0) => ({ ID, generate, hour, minutes });
+const CHANNELS = {
+    morning: CreateChannel('morning', Notifications.Mornings.generate, 0, 9),
+    //evening: CreateChannel('evening', null, 1, 19),
+    //tasks:   CreateChannel('tasks',   null, 2, 13),
 };
 
 const CHANNEL_ID = '1test';
@@ -28,7 +39,7 @@ const Management = {
         });
         if (currPerms >= 2) {
             authorization = true;
-        } else if (currPerms === 1 || (currPerms === 1 && forcePopup)) {
+        } else if (currPerms === 0 || (currPerms === 1 && forcePopup)) {
             const output = await PushNotificationIOS.requestPermissions(perms);
             if (output.authorizationStatus >= 2) authorization = true;
         }
@@ -52,27 +63,39 @@ const Management = {
     }
 };
 
-function InitMornings() {
-    if (Platform.OS === 'ios') PushNotificationIOS.removeAllPendingNotificationRequests();
-    else if (Platform.OS === 'android') PushNotification.cancelAllLocalNotifications();
+/**
+ * @param {Channel} channel
+ */
+async function Setup(channel, ) {
+    let enabled = false;
+    if (Platform.OS === 'ios') {
+        enabled = await Management.checkPermissionsIOS(); // TODO - force popup ?
+    } else if (Platform.OS === 'android') {
+        enabled = await Management.addChannelAndroid(channel.ID);
+    } else {
+        user.interface.console.AddLog('warn', 'Notifications.Setup', `Platform ${Platform.OS} is not supported`);
+    }
 
+    if (enabled) AddNotifications(channel);
+    return enabled;
+}
+
+/**
+ * @param {Channel} channel
+ */
+function AddNotifications(channel) {
+    const { ID, generate, hour, minutes } = channel;
     // Define SET_HOUR h tomorrow
     const date = new Date();
-    const hour = date.getHours();
-    if (hour > SET_HOUR) date.setDate(date.getDate() + 1);
-    date.setHours(SET_HOUR, 0, 0, 0);
+    date.setHours(hour, minutes, 0, 0);
 
     // Set all notifications for MAX_DAYS days
     for (let i = 0; i < MAX_DAYS; i++) {
-        const titles = langManager.curr['notifications']['titles'];
-        const random_title = Random(0, titles.length - 1);
-        const Title = titles[random_title];
-
-        const quote = dataManager.quotes.GetRandomQuote();
-        const Body = quote.Quote + ' (' + quote.Author + ')';
+        const { Title, Body } = generate();
 
         if (Platform.OS === 'ios') {
             PushNotificationIOS.addNotificationRequest({
+                id: ID + i.toString(),
                 title: Title,
                 body: Body,
                 fireDate: date,
@@ -80,7 +103,7 @@ function InitMornings() {
             });
         } else if (Platform.OS === 'android') {
             PushNotification.localNotificationSchedule({
-                channelId: CHANNEL_ID,
+                channelId: ID,
                 title: Title,
                 message: Body,
                 date: date
@@ -92,36 +115,18 @@ function InitMornings() {
 }
 
 /**
- * @description - Remove channel id for android and remove all pending notifications for ios
- * @param {string} channelID Used to delete android channel by ID, not used on iOS
+ * @param {Channel} channel
  */
-function Remove(channelID) {
+function Remove(channel) {
+    const { ID: channelID } = channel;
     if (Platform.OS === 'ios') {
-        PushNotificationIOS.removeAllPendingNotificationRequests();
-    } else if (Platform.OS === 'android' && !!channelID) {
+        const IDs = Range(MAX_DAYS).map(i => channelID + i.toString());
+        PushNotificationIOS.removePendingNotificationRequests(IDs);
+    } else if (Platform.OS === 'android') {
         PushNotification.channelExists(channelID, (exists) => {
             if (exists) PushNotification.deleteChannel(channelID);
         });
     }
-}
-
-async function Setup(initFunction) {
-    if (typeof(initFunction) !== 'function') {
-        user.interface.console.AddLog('warn', 'Notifications.Setup', `initFunction is not a function (${initFunction})`);
-        return false;
-    }
-
-    let enabled = false;
-    if (Platform.OS === 'ios') {
-        enabled = await Management.checkPermissionsIOS(); // TODO - force popup ?
-    } else if (Platform.OS === 'android') {
-        enabled = await Management.addChannelAndroid(CHANNEL_ID);
-    } else {
-        user.interface.console.AddLog('warn', 'Notifications.Setup', `Platform ${Platform.OS} is not supported`);
-    }
-
-    if (enabled) initFunction();
-    return enabled;
 }
 
 class Notifications {
@@ -129,8 +134,16 @@ class Notifications {
      * @description Daily notifications at 9am with a random quote to start the day
      */
     static Mornings = {
-        Enable: () => Setup(InitMornings),
-        Disable: () => Remove(CHANNELS_ID.morning)
+        Enable: () => Setup(CHANNELS.morning),
+        Disable: () => Remove(CHANNELS.morning),
+        generate: () => {
+            const titles = langManager.curr['notifications']['titles'];
+            const random_title = Random(0, titles.length - 1);
+            const Title = titles[random_title];
+            const quote = dataManager.quotes.GetRandomQuote();
+            const Body = quote.Quote + ' (' + quote.Author + ')';
+            return { Title, Body };
+        }
     };
 
     static DisableAll() {

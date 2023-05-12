dd7256541eaeeae8037f1ea70dc7291e57242f01
Gerem66 <geremy.lecaplain66@gmail.com>
2023-05-11 21:27:31 -0400

    Add jest configuration and update CI

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    preset: 'react-native',
+    setupFiles: [ './jest.setup.js' ],
+    transformIgnorePatterns: [
+      'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native|react-navigation)'
+    ],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': require.resolve('babel-jest'),
+      '\\.(xml|txt|md)$': 'jest-raw-loader',
+    },
+    moduleNameMapper: {
+      'react-native/Libraries/Image/assetPathUtils': 'react-native/Libraries/Image/assetPathUtilsWeb',
+      'react-native-web/dist/exports/Image/ImageResizeMode': 'react-native/Libraries/Image/ImageResizeMode',
+      'react-native/Libraries/Components/View/ViewStylePropTypes': 'react-native-web/dist/exports/View/ViewStylePropTypes',
+      'react-native/Libraries/Renderer/shims/ReactNativePropRegistry': 'react-native-web/dist/modules/ReactNativePropRegistry'
+    }
+};

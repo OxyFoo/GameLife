b0f7d1a5d01098d4fee8569fcc1f558a80542f3b
Gerem66 <geremy.lecaplain66@gmail.com>
2023-08-22 01:29:06 -0400

    [ScreenTuto] Updates & cleaning

---
+++ src/Interface/Components/Zap/index.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Animated, StyleSheet } from 'react-native';
+
+import InputBack from './back';
+
+/**
+ * @typedef {import('react-native').ViewStyle} ViewStyle
+ * @typedef {import('react-native').StyleProp<ViewStyle>} StyleProp
+ * @typedef {import('react-native').LayoutChangeEvent} LayoutChangeEvent
+ * 
+ * @typedef {import('Managers/ThemeManager').ColorTheme} ColorTheme
+ * @typedef {import('Managers/ThemeManager').ColorThemeText} ColorThemeText
+ */
+
+class Zap extends InputBack {
+    render() {
+        const { position, layout, orientation } = this.state;
+        const posX = Animated.subtract(position.x, layout.width / 2);
+        const posY = Animated.subtract(position.y, layout.height / 2);
+
+        const transformZap = {
+            transform: [
+                { translateX: posX },
+                { translateY: posY },
+                { scaleX: orientation === 'left' ? -1 : 1 },
+            ]
+        };
+
+        return (
+            <Animated.Image
+                style={[styles.zap, transformZap]}
+                onLayout={this.onZapLayout}
+                source={this.getZapImage()}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    zap: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: 96,
+        height: 96,
+        resizeMode: 'contain',
+    }
+});
+
+export default Zap;
---
+++ src/Interface/Widgets/ScreenTuto/style.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    parent: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    },
+    overlay: {
+        position: 'absolute',
+        borderWidth: 2,
+        borderRadius: 4
+    },
+    overlayButton: {
+        flex: 1,
+        paddingHorizontal: 0
+    },
+    hintContainer: {
+        width: '100%'
+    },
+    hint: {
+        height: '100%',
+        fontSize: 18,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        textShadowRadius: 4,
+        textShadowColor: '#000000'
+    },
+    text: {
+        position: 'absolute',
+        padding: 12,
+        borderRadius: 12,
+        borderWidth: 1,
+        backgroundColor: '#00000080',
+    },
+    textFade: {
+        fontSize: 24
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: .6,
+        backgroundColor: '#000000'
+    }
+});
+
+export default styles;

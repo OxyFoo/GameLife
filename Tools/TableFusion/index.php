75c969935d1dda6b8a0672e451d1b4e485990686
Gerem66 <geremy.lecaplain66@gmail.com>
2023-09-16 22:24:00 -0400

    [CD] Add DB migration (thanks chatGPT)

--- Tools/TableFusion/index.php
+++ Tools/TableFusion/index.php
@@ -5,8 +5,22 @@ require_once('utils.php');
 $argv = $_SERVER['argv'];
 $noConfirm = in_array('--no-confirm', $argv);
 
-$db_source = new mysqli('localhost', 'username', 'password', 'database');
-$db_target = new mysqli('localhost', 'username', 'password', 'database');
+if ($argc < 9) {
+    echo "Usage: php index.php <source_host> <source_user> <source_pass> <source_db> <target_host> <target_user> <target_pass> <target_db> [--no-confirm]\n";
+    exit(1);
+}
+
+$source_host = $argv[1];
+$source_user = $argv[2];
+$source_pass = $argv[3];
+$source_db = $argv[4];
+$target_host = $argv[5];
+$target_user = $argv[6];
+$target_pass = $argv[7];
+$target_db = $argv[8];
+
+$db_source = new mysqli($source_host, $source_user, $source_pass, $source_db);
+$db_target = new mysqli($target_host, $target_user, $target_pass, $target_db);
 
 if ($db_source->connect_error || $db_target->connect_error) {
     die("Connection failed: " . $db_source->connect_error . " / " . $db_target->connect_error);

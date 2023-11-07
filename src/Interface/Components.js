10cca00fd8095d2d1790fae2e6060039ea0ff7d9
AphroMad <pierre.marsaa@wanadoo.fr>
2023-11-07 22:07:52 +0100

    delete bar chart for now

--- src/Interface/Components.js
+++ src/Interface/Components.js
@@ -21,7 +21,6 @@ import TextSwitch from './Components/TextSwitch';
 import XPBar from './Components/XPBar';
 import Zap from './Components/Zap';
 import PieChart from './Components/PieChart';
-import BarChart from './Components/BarChart';
 
 export {
     Button,
@@ -47,5 +46,4 @@ export {
     XPBar,
     Zap,
     PieChart,
-    BarChart
 }

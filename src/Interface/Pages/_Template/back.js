3ace83d1a68c2827c61297451d71d34a95337a76
Gerem66 <geremy.lecaplain66@gmail.com>
2023-12-08 18:06:22 +0100

    [Pages] Add template page

---
+++ src/Interface/Pages/_Template/back.js
@@ -0,0 +1,12 @@
+import { PageBase } from 'Interface/Components';
+
+class BackQuests extends PageBase {
+    state = {
+    }
+
+    componentDidMount() {
+        super.componentDidMount();
+    }
+}
+
+export default BackQuests;

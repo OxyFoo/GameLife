c8f0f9d59981ff32b559e7a91c0556f8424a5c1f
Gerem <geremy.lecaplain66@gmail.com>
2024-01-05 19:41:02 +0100

    [BundleSwitcher] Cleaning

--- Tools/BundleSwitcher/bundleSwitcher.sh
+++ Tools/BundleSwitcher/bundleSwitcher.sh
@@ -13,49 +13,46 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# If not Linux or macOS, exit
+if [ "$(uname)" != "Linux" ] && [ "$(uname)" != "Darwin" ]; then
+    echo -e "Unsupported platform: $(uname)"
+    echo -e "Supported platforms: Linux, macOS"
+    exit 1
+fi
+
 if [ "$1" == "test" ]; then
+    # Android
+    echo -e "\tAndroid"
+    echo -e "New Bundle ID: com.gamelife.test"
+    echo -ne "Replacing..."
     if [ "$(uname)" == "Linux" ]; then
-        # Android
-        echo -e "\tAndroid"
-        echo -e "New Bundle ID: com.gamelife.test"
-        echo -ne "Replacing..."
         EDITED_FILES=$(find ./ \
             -type d -name res -prune -o \
             -type d -name Tools -prune -o \
             -type d -name node_modules -prune -o \
             -type f -regex '.*\.\(gradle\|pro\|xml\|js\|java\)' \
             -exec sh -c 'grep -q "com.gamelife" "{}" && sed -i "s/com.gamelife/com.gamelife.test/g" "{}" && echo "{}"' \;)
-        echo -e "\rEdited files: $(echo "$EDITED_FILES" | wc -l)"
-
-        # iOS
-        echo -e "\n\tiOS"
-        echo -e "New Bundle ID: GameLife.test"
-        echo -ne "Replacing..."
-        sed -i 's/PRODUCT_NAME = GameLife;/PRODUCT_NAME = GameLife.test;/g' ./ios/GameLife.xcodeproj/project.pbxproj
-        echo -e "\rEdited files: 1"
     elif [ "$(uname)" == "Darwin" ]; then
-        # Android
-        echo -e "\tAndroid"
-        echo -e "New Bundle ID: com.gamelife.test"
-        echo -ne "Replacing..."
         EDITED_FILES=$(find ./ \
             -type d -name res -prune -o \
             -type d -name Tools -prune -o \
             -type d -name node_modules -prune -o \
             -type f -regex '.*\.\(gradle\|pro\|xml\|js\|java\)' \
             -exec sh -c 'grep -q "com.gamelife" "{}" && sed -i '' "s/com.gamelife/com.gamelife.test/g" "{}" && echo "{}"' \;)
-        echo -e "\rEdited files: $(echo "$EDITED_FILES" | wc -l)"
-
-        # iOS
-        echo -e "\n\tiOS"
-        echo -e "New Bundle ID: GameLife.test"
-        echo -ne "Replacing..."
-        sed -i '' 's/PRODUCT_NAME = GameLife;/PRODUCT_NAME = GameLife.test;/g' ./ios/GameLife.xcodeproj/project.pbxproj
-        echo -e "\rEdited files: 1"
-    else
-        echo "Unsupported OS."
-        exit 1
     fi
+    echo -e "\rEdited files: $(echo "$EDITED_FILES" | wc -l)"
+
+    # iOS
+    echo -e "\n\tiOS"
+    echo -e "New Bundle ID: GameLife.test"
+    echo -ne "Replacing..."
+
+    if [ "$(uname)" == "Linux" ]; then
+        sed -i 's/PRODUCT_NAME = GameLife;/PRODUCT_NAME = GameLife.test;/g' ./ios/GameLife.xcodeproj/project.pbxproj
+    elif [ "$(uname)" == "Darwin" ]; then
+        sed -i '' 's/PRODUCT_NAME = GameLife;/PRODUCT_NAME = GameLife.test;/g' ./ios/GameLife.xcodeproj/project.pbxproj
+    fi
+    echo -e "\rEdited files: 1"
 else
     echo "Unsupported environment."
     exit 1

5d165d5d66b273c9781a491a0c7a4e55db2bc7bb
Gerem <geremy.lecaplain66@gmail.com>
2024-01-15 18:45:37 +0100

    [CICD] Attempt 3

--- Tools/BundleSwitcher/bundleSwitcher.sh
+++ Tools/BundleSwitcher/bundleSwitcher.sh
@@ -49,10 +49,14 @@ if [ "$1" == "test" ]; then
 
     if [ "$(uname)" == "Linux" ]; then
         sed -i 's/PRODUCT_NAME = GameLife;/PRODUCT_NAME = GameLife.test;/g' ./ios/GameLife.xcodeproj/project.pbxproj
+        sed -i 's/<string>org.reactjs.native.example.GameLife/<string>org.reactjs.native.example.GameLife.test/g' ./ios/GoogleService-Info.plist
+        sed -i 's/94a736940ff60c834b1898/22189fa611f1a9d64b1898/g' ./ios/GoogleService-Info.plist
     elif [ "$(uname)" == "Darwin" ]; then
         sed -i '' 's/PRODUCT_NAME = GameLife;/PRODUCT_NAME = GameLife.test;/g' ./ios/GameLife.xcodeproj/project.pbxproj
+        sed -i '' 's/<string>org.reactjs.native.example.GameLife/<string>org.reactjs.native.example.GameLife.test/g' ./ios/GoogleService-Info.plist
+        sed -i '' 's/94a736940ff60c834b1898/22189fa611f1a9d64b1898/g' ./ios/GoogleService-Info.plist
     fi
-    echo -e "\rEdited files: 1"
+    echo -e "\rEdited files: 2"
 else
     echo "Unsupported environment."
     exit 1

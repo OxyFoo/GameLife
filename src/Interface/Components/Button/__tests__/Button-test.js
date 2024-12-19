84c0dcf4bef45101724ec6c15925d741c2375064
Gerem66 <geremy.lecaplain66@gmail.com>
2024-12-19 17:56:30 -0500

    [Tests] Fix

--- src/Interface/Components/Button/__tests__/Button-test.js
+++ src/Interface/Components/Button/__tests__/Button-test.js
@@ -3,37 +3,44 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
 // Note: test renderer must be required after react-native.
-import renderer, { create } from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 // Graphical components
 import { Button } from '../index';
 
 describe('[Component] Button', () => {
     it('renders correctly', () => {
-        const tree = renderer
-            .create(<Button>Test</Button>)
-            .toJSON();
+        const tree = renderer.create(<Button>Test</Button>).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('event onPress', () => {
         const onPress = jest.fn();
-        const { root } = create(<Button onPress={onPress}>Test</Button>);
+        const { getByTestId } = render(
+            <Button testID='button' onPress={onPress}>
+                Test
+            </Button>
+        );
 
-        fireEvent.press(root);
+        const button = getByTestId('button');
 
-        expect(root).toBeTruthy();
+        // Simulez les événements tactiles
+        fireEvent(button, 'touchStart', {
+            nativeEvent: { pageX: 100, pageY: 100, locationX: 50, locationY: 50 }
+        });
+
+        fireEvent(button, 'touchEnd', {
+            nativeEvent: { pageX: 100, pageY: 100 }
+        });
+
+        // Vérifiez si onPress a été appelé
         expect(onPress).toHaveBeenCalledTimes(1);
     });
 
     it('event onPress disabled', () => {
         const onPress = jest.fn();
         const { getByTestId } = render(
-            <Button
-                testID='button'
-                onPress={onPress}
-                enabled={false}
-            >
+            <Button testID='button' onPress={onPress} enabled={false}>
                 Test
             </Button>
         );

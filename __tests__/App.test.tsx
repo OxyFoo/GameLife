31756ac93f8634cee6d0b39bf13e1f9de7b1ffdc
Gerem66 <geremy.lecaplain66@gmail.com>
2025-01-28 12:21:49 -0500

    [RN] Upgrade to 0.77.0

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,22 +2,16 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-jest.useFakeTimers();
-
-it('renders correctly', () => {
-  //renderer.create(<App />);
+test('renders correctly', async () => {
+    await ReactTestRenderer.act(() => {
+        ReactTestRenderer.create(<App />);
+    });
 });
 
 afterEach(() => {
-  jest.clearAllTimers();
+    jest.clearAllTimers();
 });

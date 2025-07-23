bc184b1ccf02e8a019cc74470b5f21a2f9146ddc
Gerem66 <geremy.lecaplain66@gmail.com>
2025-07-22 20:51:33 -0400

    refactor(zapgpt): Delete files and exports linked to ZAPGPT

--- src/Interface/Widgets.js
+++ src/Interface/Widgets.js
@@ -13,6 +13,5 @@ import StatsBars from './Widgets/StatsBars';
 import SkillChart from './Widgets/SkillChart';
 import SkillsGroup from './Widgets/SkillsGroup';
 import TodayPieChart from './Widgets/TodayPieChart';
-import ZapGPT from './Widgets/ZapGPT';
 
-export { BlockMonth, StatsBars, SkillChart, SkillsGroup, TodayPieChart, ZapGPT };
+export { BlockMonth, StatsBars, SkillChart, SkillsGroup, TodayPieChart };

d5e402e9a0ff6392c26d2acfe035216bedde8b9e
Gerem66 <geremy.lecaplain66@gmail.com>
2025-12-21 01:35:26 -0500

    feat(zap): added Christmas skin for the end of year holidays

--- res/zap/zap.js
+++ res/zap/zap.js
@@ -2,26 +2,52 @@
 
 const ZAP_IMAGES = {
     day: {
-        onTwoLegs: {
-            face: require('Ressources/zap/purple/up.png'),
-            show: require('Ressources/zap/purple/upShow.png')
+        normal: {
+            onTwoLegs: {
+                face: require('Ressources/zap/purple/normal/up.png'),
+                show: require('Ressources/zap/purple/normal/upShow.png')
+            },
+            onFourLegs: {
+                face: require('Ressources/zap/purple/normal/down.png'),
+                show: require('Ressources/zap/purple/normal/downShow.png')
+            },
+            high: require('Ressources/zap/purple/normal/high.png')
         },
-        onFourLegs: {
-            face: require('Ressources/zap/purple/down.png'),
-            show: require('Ressources/zap/purple/downShow.png')
-        },
-        high: require('Ressources/zap/purple/high.png')
+        christmas: {
+            onTwoLegs: {
+                face: require('Ressources/zap/purple/christmas/up.png'),
+                show: require('Ressources/zap/purple/christmas/upShow.png')
+            },
+            onFourLegs: {
+                face: require('Ressources/zap/purple/christmas/down.png'),
+                show: require('Ressources/zap/purple/christmas/downShow.png')
+            },
+            high: require('Ressources/zap/purple/christmas/high.png')
+        }
     },
     night: {
-        onTwoLegs: {
-            face: require('Ressources/zap/black/up.png'),
-            show: require('Ressources/zap/black/upShow.png')
+        normal: {
+            onTwoLegs: {
+                face: require('Ressources/zap/black/normal/up.png'),
+                show: require('Ressources/zap/black/normal/upShow.png')
+            },
+            onFourLegs: {
+                face: require('Ressources/zap/black/normal/down.png'),
+                show: require('Ressources/zap/black/normal/downShow.png')
+            },
+            high: require('Ressources/zap/black/normal/high.png')
         },
-        onFourLegs: {
-            face: require('Ressources/zap/black/down.png'),
-            show: require('Ressources/zap/black/downShow.png')
-        },
-        high: require('Ressources/zap/black/high.png')
+        christmas: {
+            onTwoLegs: {
+                face: require('Ressources/zap/black/christmas/up.png'),
+                show: require('Ressources/zap/black/christmas/upShow.png')
+            },
+            onFourLegs: {
+                face: require('Ressources/zap/black/christmas/down.png'),
+                show: require('Ressources/zap/black/christmas/downShow.png')
+            },
+            high: require('Ressources/zap/black/christmas/high.png')
+        }
     }
 };
 

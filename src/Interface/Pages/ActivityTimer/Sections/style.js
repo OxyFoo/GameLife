91ab012ed5c9328f828f04050ce744c2a69dc12f
Gerem66 <geremy.lecaplain66@gmail.com>
2026-01-15 02:05:06 +0100

    refactor(style): dead code

--- src/Interface/Pages/ActivityTimer/Sections/style.js
+++ src/Interface/Pages/ActivityTimer/Sections/style.js
@@ -2,9 +2,6 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     // Title
-    gradient: {
-        borderRadius: 8
-    },
     gradientInner: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -31,39 +28,6 @@ const styles = StyleSheet.create({
     durationText: {
         fontSize: 64,
         fontWeight: 'bold'
-    },
-
-    // Score
-    scoreParent: {
-        marginBottom: 16,
-        borderRadius: 8
-    },
-    scoreView: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 16
-    },
-    scoreTitle: {
-        fontSize: 22,
-        fontWeight: 'bold'
-    },
-    scoreCell: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginRight: 12,
-        marginBottom: 12
-    },
-    scoreCellSquare: {
-        borderRadius: 8
-    },
-    scoreCellSquareInner: {
-        padding: 16
-    },
-    scoreCellText: {
-        fontSize: 16,
-        marginLeft: 12
     },
 
     // Friends
